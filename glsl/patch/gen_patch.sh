7b89d97a27cde131a00d7568d5eb6f8e634f70d7
Syoyo Fujita <syoyo@lighttransport.com>
2015-01-20 18:52:43 +0900

    Improve procedure for Mesa patch generation. Introduce new GLSL ext function(numIntersects() and quertyIntersect())

--- glsl/patch/gen_patch.sh
+++ glsl/patch/gen_patch.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 olddir=`pwd`
-
-cd ~/src
-diff -crN Mesa-9.0.1.org/src/glsl Mesa-9.0.1.lsgl/src/glsl > "${olddir}"/mesa_glsl_fix.patch
-
+cd build
+rm -rf Mesa-9.0.1.org
+mkdir Mesa-9.0.1.org
+tar -jxvf ../../deps/MesaLib-9.0.1.tar.bz2 -C Mesa-9.0.1.org --strip-components=1
+diff -crN Mesa-9.0.1.org/src/glsl Mesa-9.0.1/src/glsl > "${olddir}"/mesa_glsl_fix.patch
 cd $olddir
