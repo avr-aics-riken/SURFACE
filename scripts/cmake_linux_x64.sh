524511b2e96dd3d1acf6878a030fe13568de737a
Syoyo Fujita <syoyo@lighttransport.com>
2015-03-03 00:49:59 +0900

    Read CXX and CC from environment variable if it set.

--- scripts/cmake_linux_x64.sh
+++ scripts/cmake_linux_x64.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 
 if [ -z "${CMAKE_BIN+x}" ]; then
-  CMAKE_BIN=cmake28
+  CMAKE_BIN=cmake
+fi
+
+if [ -z "${CXX+x}" ]; then
+  CXX=g++
+fi
+
+if [ -z "${CC+x}" ]; then
+  CC=gcc
 fi
 
 rm -rf build
 echo ${CMAKE_BIN}
-${CMAKE_BIN} -H. -DCMAKE_INSTALL_PREFIX=dist -DSURFACE_BUILD_WITH_OPENMP=On -DBUILD_SHARED_LIBS=On -DCMAKE_BUILD_TYPE=Release -Bbuild
+${CMAKE_BIN} -H. -DCMAKE_INSTALL_PREFIX=dist -DCMAKE_C_COMPIER=${CC} -DCMAKE_CXX_COMPILER=${CXX} -DSURFACE_BUILD_WITH_OPENMP=On -DBUILD_SHARED_LIBS=On -DCMAKE_BUILD_TYPE=Release -Bbuild
 
 #CC=gcc-4.8.4 CXX=g++-4.8.4 ${CMAKE_BIN} -H. -DCMAKE_INSTALL_PREFIX=dist -DSURFACE_BUILD_WITH_OPENMP=On -DBUILD_SHARED_LIBS=On -DCMAKE_BUILD_TYPE=Release -Bbuild
--- scripts/cmake_macosx.sh
+++ scripts/cmake_macosx.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
-cmake -DCMAKE_CXX_COMPILER=g++-4.8 -DCMAKE_C_COMPILER=gcc-4.8 -DCMAKE_INSTALL_PREFIX=dist -H. -DBUILD_SHARED_LIBS=Off -DCMAKE_BUILD_TYPE=Release -Bbuild
+if [ -z "${CMAKE_BIN+x}" ]; then
+  CMAKE_BIN=cmake
+fi
+
+if [ -z "${CXX+x}" ]; then
+  CXX=g++
+fi
+
+if [ -z "${CC+x}" ]; then
+  CC=gcc
+fi
+
+${CMAKE_BIN} -DCMAKE_CXX_COMPILER=${CXX} -DCMAKE_C_COMPILER=${CC} -DCMAKE_INSTALL_PREFIX=dist -H. -DBUILD_SHARED_LIBS=Off -DCMAKE_BUILD_TYPE=Release -Bbuild
