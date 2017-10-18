6ca8622b8ead82806e85ad51508559faf1b4ed38
Syoyo Fujita <syoyo@lighttransport.com>
2017-10-18 15:08:28 +0900

    Update sparc64 build of Mesa glsl_compiler

--- glsl/patch/README.txt
+++ glsl/patch/README.txt
@@ -6,3 +6,8 @@ setup_patched_mesa.sh
 
 パッチの当てられた Mesa source tree を生成するスクリプト.
 src/glsl で scons -u とすると glsl_compiler(or glsl2) が生成される.
+
+
+# NOTE
+
+Please use scons 2.4.1. scons 2.5 or later does not work.
---
+++ glsl/patch/build_patched_mesa_k_native.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+olddir=`pwd`
+cd build/Mesa-9.0.1/src/glsl/
+scons -u -c
+scons -u build=release
+if [ -f ${olddir}/build/Mesa-9.0.1/build/linux/glsl/glsl2 ]; then
+	cp ${olddir}/build/Mesa-9.0.1/build/linux/glsl/glsl2 ${olddir}/../bin/sparc64/glsl_compiler
+fi
+cd $olddir
